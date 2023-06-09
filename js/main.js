799f2295dbdcd3951ddc798d7749492286be3b2b
nikollllllas <nikollasohta2014@gmail.com>
2023-06-09 01:44:01 -0300

    layout and script adjusts

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
 function clearOutput() {
-    document.getElementById('output').textContent = '';
+    document.getElementById('output').textContent = ''
 }
 
 function appendToOutput(value) {
-    document.getElementById('output').textContent += value;
+    document.getElementById('output').textContent += value
 }
 
 function calculateResult() {
-    const input = document.getElementById('output').textContent;
+    const input = document.getElementById('output').textContent
 
-    const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/;
+    const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/
 
     if (!operationRegex.test(input)) {
-        document.getElementById('output').textContent = 'Operação inválida';
-        return;
+        document.getElementById('output').textContent = 'Operação inválida'
+
+        return
     }
 
     try {
-        let result = eval(input.replace('%', '/100'));
-        document.getElementById('output').textContent = result;
+        let result = eval(input.replace('%', '/100'))
+        document.getElementById('output').textContent = result
     } catch (error) {
-        document.getElementById('output').textContent = 'Erro de cálculo';
+        document.getElementById('output').textContent = 'Erro de cálculo'
     }
 }
