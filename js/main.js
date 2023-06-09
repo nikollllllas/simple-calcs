990e03dcfc42c87f0773d35c7c25c15c3343a9d9
nikollllllas <nikollasohta2014@gmail.com>
2023-06-09 01:22:35 -0300

    add functions

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function clearOutput() {
+    document.getElementById('output').textContent = '';
+}
+
+function appendToOutput(value) {
+    document.getElementById('output').textContent += value;
+}
+
+function calculateResult() {
+    const input = document.getElementById('output').textContent;
+
+    const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/;
+
+    if (!operationRegex.test(input)) {
+        document.getElementById('output').textContent = 'Operação inválida';
+        return;
+    }
+
+    try {
+        let result = eval(input.replace('%', '/100'));
+        document.getElementById('output').textContent = result;
+    } catch (error) {
+        document.getElementById('output').textContent = 'Erro de cálculo';
+    }
+}
