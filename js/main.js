46713c27e3427b581dfb827d615dda87f7e5c41d
nikollllllas <nikollasohta2014@gmail.com>
2023-06-09 18:54:45 -0300

    main.js w/ perccent error

--- js/main.js
+++ js/main.js
@@ -1,76 +1,37 @@
 function clearOutput() {
-    document.getElementById('output').textContent = ''
+    document.getElementById('output').textContent = '' 
 }
 
-function appendToOutput(value) {
-    document.getElementById('output').textContent += value
+function printToOutput(value) {
+    document.getElementById('output').textContent += value 
+}
+
+function deleteLastCharacter() {
+    const output = document.getElementById('output') 
+    const currentContent = output.textContent 
+
+    if (currentContent.length > 0) {
+        output.textContent = currentContent.slice(0, -1) 
+    }
 }
 
 function calculateResult() {
-    const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/
-    const input = document.getElementById('output').textContent
+    const input = document.getElementById('output').textContent 
+    const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/ 
 
     if (!operationRegex.test(input)) {
-        document.getElementById('output').textContent = 'Operação inválida'
-
-        return
+        document.getElementById('output').textContent = 'Operação inválida' 
+        return 
     }
 
-    let result
-    
+    let result 
     try {
-        result = evaluateExpression(input)
-        document.getElementById('output').textContent = result
+        const modifiedInput = input.replace(/(\d+(\.\d+)?)%/g, (match, number) => {
+            return parseFloat(number) / 100 
+        }) 
+        result = eval(modifiedInput) 
+        document.getElementById('output').textContent = result 
     } catch (error) {
-        document.getElementById('output').textContent = 'Erro de cálculo'
-    }
-}
-
-function evaluateExpression(expression) {
-    let result = 0
-    let currentOperator = null
-
-    const tokens = expression.match(/(-?\d+(\.\d+)?|[+\-*\/%])/g)
-
-    for (let i = 0; i < tokens.length; i++) {
-        const token = tokens[i]
-
-        if (isNumber(token)) {
-            const number = parseFloat(token)
-
-            if (currentOperator === null) {
-                result = number
-            } else {
-                result = applyOperator(result, currentOperator, number)
-            }
-        } else {
-            currentOperator = token
-        }
-    }
-
-    return result
-}
-
-function isNumber(token) {
-    return !isNaN(parseFloat(token)) && isFinite(token)
-}
-
-function applyOperator(operand1, operator, operand2) {
-    switch (operator) {
-        case '+':
-            return operand1 + operand2
-        case '-':
-            return operand1 - operand2
-        case '*':
-            return operand1 * operand2
-        case '/':
-            if (operand2 === 0) {
-                throw new Error('Divisão por zero')
-            }
-            return operand1 / operand2
-        case '%':
-            return operand1 * (operand2 / 100)
-        default:
-            throw new Error('Operador inválido')
+        document.getElementById('output').textContent = 'Erro de cálculo' 
     }
 }
