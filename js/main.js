30c209eeffb8914e03155b974af9b27f33220db4
nikollllllas <nikollasohta2014@gmail.com>
2023-06-09 15:13:14 -0300

    main.js w/ subtract error

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ function appendToOutput(value) {
 }
 
 function calculateResult() {
-    const input = document.getElementById('output').textContent
-
     const operationRegex = /^-?\d+(\.\d+)?(?:[+\-*\/%]-?\d+(\.\d+)?)*$/
+    const input = document.getElementById('output').textContent
 
     if (!operationRegex.test(input)) {
         document.getElementById('output').textContent = 'Operação inválida'
@@ -17,10 +16,61 @@ function calculateResult() {
         return
     }
 
+    let result
+    
     try {
-        let result = eval(input.replace('%', '/100'))
+        result = evaluateExpression(input)
         document.getElementById('output').textContent = result
     } catch (error) {
         document.getElementById('output').textContent = 'Erro de cálculo'
+    }
+}
+
+function evaluateExpression(expression) {
+    let result = 0
+    let currentOperator = null
+
+    const tokens = expression.match(/(-?\d+(\.\d+)?|[+\-*\/%])/g)
+
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i]
+
+        if (isNumber(token)) {
+            const number = parseFloat(token)
+
+            if (currentOperator === null) {
+                result = number
+            } else {
+                result = applyOperator(result, currentOperator, number)
+            }
+        } else {
+            currentOperator = token
+        }
+    }
+
+    return result
+}
+
+function isNumber(token) {
+    return !isNaN(parseFloat(token)) && isFinite(token)
+}
+
+function applyOperator(operand1, operator, operand2) {
+    switch (operator) {
+        case '+':
+            return operand1 + operand2
+        case '-':
+            return operand1 - operand2
+        case '*':
+            return operand1 * operand2
+        case '/':
+            if (operand2 === 0) {
+                throw new Error('Divisão por zero')
+            }
+            return operand1 / operand2
+        case '%':
+            return operand1 * (operand2 / 100)
+        default:
+            throw new Error('Operador inválido')
     }
 }
