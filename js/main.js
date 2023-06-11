9ea749e900deb5e8c8b2bdfdc46bb9145ae71f53
nikollllllas <nikollasohta2014@gmail.com>
2023-06-11 18:06:44 -0300

    while loop to do percent operation

--- js/main.js
+++ js/main.js
@@ -1,33 +1,56 @@
 function clearOutput() {
-    document.getElementById('output').textContent = '' 
+    document.getElementById('output').innerHTML = '' 
 }
 
 function printToOutput(value) {
-    document.getElementById('output').textContent += value 
+    document.getElementById('output').innerHTML += value 
 }
 
 function deleteLastCharacter() {
     const output = document.getElementById('output') 
-    const currentContent = output.textContent 
+    const currentContent = output.innerHTML 
 
     if (currentContent.length > 0) {
-        output.textContent = currentContent.slice(0, -1) 
+        output.innerHTML = currentContent.slice(0, -1) 
     }
 }
 
 function calculateResult() {
-    const input = document.getElementById('output').textContent 
-    const operationRegex = /^-?\d+(\.\d+)?(?:-[+\/*]\d+(\.\d+)?%?)*$/
+    const output = document.getElementById("output");
+    const expression = output.innerHTML;
 
-    let result 
-    try {
-        const modifiedInput = input.replace(/(\d+(\.\d+)?)%/g, (number) => {
-        return parseFloat(number) / 100
-    })
-    result = eval(modifiedInput);
-    document.getElementById('output').textContent = result;
-    } catch (error) {
-        document.getElementById('output').textContent = 'Erro de cálculo';
+    const regex = /^(\d+(\.\d+)?)([\+\-\*\/%]?)(\d+(\.\d+)?%?)$/;
+
+    if (regex.test(expression)) {
+        try {
+            let result = expression;
+
+            while (result.includes("%")) {
+                const percentIndex = result.indexOf("%");
+                const numberIndex = result.lastIndexOf(/[+\-*\/]/, percentIndex);
+
+                const number = parseFloat(result.substring(numberIndex + 1, percentIndex));
+                const percent = parseFloat(result.substring(percentIndex + 1, result.length - 1));
+
+                const calculatedValue = number * (percent / 100);
+                result = result.substring(0, numberIndex + 1) + calculatedValue + result.substring(result.length - 1);
+                console.log('Fez a operação')
+            }
+
+            const finalResult = eval(result);
+
+            if (finalResult === Infinity) {
+                alert("Divisão por zero não é permitida!");
+                clearOutput();
+            } else {
+                output.innerHTML = finalResult;
+            }
+        } catch (error) {
+            alert("Ocorreu um erro ao calcular a expressão!");
+            clearOutput();
+        }
+    } else {
+        alert("Operação inválida!");
+        clearOutput();
     }
-
 }
